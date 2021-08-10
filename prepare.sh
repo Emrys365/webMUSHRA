044b1eac263c4d0f6493c9709b9fd709619bd41b
Wangyou Zhang <C0me_On@163.com>
2021-08-11 01:37:03 +0800

    Finalize documentation for enh_page; Support changing rating text when switching metric tabs

--- prepare.sh
+++ prepare.sh
@@ -4,20 +4,21 @@
 # Prepare configs for CHiME4 data #
 ###################################
 
-# Since webMUSHRA needs stereo audio, we convert the monouranl audio to stereo.
+# Since webMUSHRA needs stereo audio, we convert the monaural audio to stereo.
 if [ ! -L configs/resources/wavs ]; then
     ln -s "$(realpath ../wavs)" configs/resources/wavs
 fi
 ./bin/convert_mono_to_stereo.sh configs/resources/wavs/ configs/resources/wavs_stereo/
 
 # Generate config
-./bin/generate_enh_config_en.py \
+./bin/generate_enh_config.py \
     --sample_audio_path configs/resources/wavs_stereo/close_talk/F05_440C0206_STR_REAL.wav \
     --seed 777 \
+    --language en \
     --metrics MOS,S-MOS,N-MOS \
     --random True \
     --ref_root_wav_dir ./configs/resources/wavs_stereo/close_talk \
-    --test_root_wav_dir ./configs/resources/wavs_stereo/{wav,mc_conv_tasnet,beam_tasnet_sig,beam_tasnet_vad_mask,joint_mc_conv_tasnet_asr} \
+    --test_root_wav_dir ./configs/resources/wavs_stereo/{wav,blstm_mvdr,fasnet,mc_conv_tasnet,beam_tasnet_sig,beam_tasnet_vad_mask,joint_mc_conv_tasnet_asr} \
     --outpath ./configs/enh_quality_MOS_sample.yaml
 
 echo "Run the following command to launch WebMUSHRA:"
